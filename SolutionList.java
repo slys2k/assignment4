32b7ad20d8c362d581087f81519d3b5bb6fd0ac5
slys2k <keithcyr@utexas.edu>
2014-03-26 22:02:49 -0500

    SolutionList

--- SolutionList.java
+++ SolutionList.java
@@ -1,12 +1,15 @@
 package assignment4;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /** This class represents a possible solution list  used by a recursive method. 
   * The purpose of this class is to maintain a progressive list until the
   * recursion either finds a solution or determines that no solution is possible.
   *
   * @author K. Cyr, J. Ngo, S. Cooney, K. Subramanian
   */
-public class solutionList
+public class SolutionList
 {
    // Data Fields
 
@@ -24,9 +27,8 @@ public class solutionList
 
    /** Construct a solutionList with an empty ladder marked incomplete.
      */
-   public solutionList()
+   public SolutionList()
    {
-      ladder_size = 0;
       complete = false;
    }
 
@@ -50,7 +52,7 @@ public class solutionList
    /** Gets the solutionList
      * @return The entire solution list.
      */
-   public int getWordLadder() { return wordLadder; }
+   public ArrayList<String> getWordLadder() { return wordLadder; }
 
    // Other Methods
    
